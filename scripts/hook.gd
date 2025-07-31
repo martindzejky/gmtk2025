d06744cbf0b792cae5a7b7a55f7aa9c27d9be0ee
Martin Jakubik <martindzejky@gmail.com>
2025-07-31 09:00:45 +0200

    implement looping and catching

--- scripts/hook.gd
+++ scripts/hook.gd
@@ -3,7 +3,9 @@ class_name Hook
 
 @export var player: Player
 @export var lasso: Lasso
+@export var progress_bar: ProgressBar
 @export var max_distance_from_player := 100.0
+@export var speed := 300.0
 
 enum State {
   WITH_PLAYER,
@@ -14,7 +16,9 @@ enum State {
 
 var state: State = State.WITH_PLAYER
 var target_direction: Vector2 = Vector2.ZERO
-var speed := 300.0
+var total_angle_change := 0.0
+var starting_angle := 0.0
+var last_angle := 0.0
 
 func shoot_in_direction(new_target_direction: Vector2):
   if state != State.WITH_PLAYER:
@@ -27,9 +31,27 @@ func shoot_in_direction(new_target_direction: Vector2):
 
   print('Shooting in direction: ', target_direction)
 
+func hook_to_enemy(enemy: Enemy):
+  if state != State.FLYING:
+    print('Hook is not flying so it cannot hook to enemy')
+    return
+
+  state = State.HOOKED
+
+  print('Hooking to enemy: ', enemy)
+  reparent(enemy)
+  position = Vector2.ZERO
+  total_angle_change = 0.0
+  starting_angle = global_position.angle_to_point(player.global_position)
+  last_angle = starting_angle
+
+func catch_enemy(enemy: Enemy):
+  # TODO: what happens here?
+  enemy.queue_free()
+
 func unhook():
   if state != State.HOOKED:
-    print('Hook is not hooked so it cannot unhook')
+    print('Hook is not hooked so it cannot unhook') # so many hooks...
     return
 
   state = State.RETURNING
@@ -37,6 +59,8 @@ func unhook():
 
 func _process(delta):
   queue_redraw()
+
+  progress_bar.visible = state == State.HOOKED
 
   match state:
     State.FLYING:
@@ -54,10 +78,34 @@ func _process(delta):
         reparent(lasso)
         position = Vector2.ZERO
 
+    State.HOOKED:
+      var current_angle := global_position.angle_to_point(player.global_position)
+      var angle_diff := current_angle - last_angle
+
+      # handle the -PI to PI angle wrap
+      if angle_diff > PI:
+        angle_diff -= 2 * PI
+      elif angle_diff < -PI:
+        angle_diff += 2 * PI
+
+      total_angle_change += angle_diff
+      last_angle = current_angle
+
+      # have we wrapped around yet?
+      if abs(total_angle_change) >= 2 * PI:
+        catch_enemy(get_parent())
+        unhook()
+
+      progress_bar.value = abs(total_angle_change) / (2 * PI)
+
 func _draw():
   if state != State.WITH_PLAYER:
     # draw chain to the player
     draw_line(Vector2.ZERO, player.global_position - global_position, Color.WHITE, 2.0)
+
+  if state == State.HOOKED:
+    # draw the starting direction
+    draw_line(Vector2.ZERO, Vector2.RIGHT.rotated(starting_angle) * 100, Color.PINK, 2.0)
 
 
 func _on_body_entered(body: Node2D):
@@ -66,10 +114,6 @@ func _on_body_entered(body: Node2D):
   if state != State.FLYING: return
 
   if body is Enemy:
-    state = State.HOOKED
-
-    print('Hooking to enemy: ', body)
-    call_deferred('reparent', body)
-    call_deferred('set_position', Vector2.ZERO)
+    call_deferred('hook_to_enemy', body)
   else:
     state = State.RETURNING
