fd5a46dce9b1d445489a3bdf5210f12881e137c2
Martin Jakubik <martindzejky@gmail.com>
2025-08-03 13:00:08 +0200

    hide the upgrade UI

--- scripts/wave_ends.gd
+++ scripts/wave_ends.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@export var display_timer: Timer
 @export var text: Label
 
 func _ready():
@@ -8,9 +9,13 @@ func _ready():
 
 func _on_wave_ends(number: int):
   visible = true
-  get_tree().paused = true
+  # get_tree().paused = true # TODO: enable if I get to upgrades
+  display_timer.start()
   text.text = 'Wave %d complete!' % (number + 1)
 
 func _on_continue_pressed() -> void:
-  get_tree().paused = false
+  # get_tree().paused = false # TODO: enable if I get to upgrades
+  visible = false
+
+func _on_display_timer_timeout():
   visible = false
